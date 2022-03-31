518a48537f61b48fa6905a7f63d3fa8343768330
celialali <celia.rdc31@gmail.com>
2022-03-31 14:09:56 +0200

    ajout de tout

---
+++ entete.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="stylesheet.css"/>
+        <title> Envol CESSNA </title>
+    </head>
+    <body>
+
+    <nav class="navbar">
+        <div class="logo">
+            <img src="images/avion.png" alt="Avion" height="30" width="66">
+        </div>
+        <a href="#" class="pres links">Présentation</a>
+        <a href="#" class="gestion links">Gestion de projet</a>
+        <a href="#" class="livrables links">Livrables</a>
+        <a href="#" class="PlanVert links">Plan Vert</a>
+
+    </nav>
