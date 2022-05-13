182ec3066140ef97046e7f02ebb2d802a129e8e0
celialali <celia.rdc31@gmail.com>
2022-05-13 15:59:46 +0200

    php

---
+++ cessna.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<?php include_once('entete.php');?>
+
+<h1><span><img src="images/logo_Envol_Cessna.png" alt="Avion" ></span> </h1>
+<h2>Projet transdisciplinaire : simulateur d'avion civil</h2>
+
+
+<button>À propos de nous</button>
+</body>
+
+</html>  
---
+++ presentation.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<?php include_once('entete.php');?>
+
+    
+    <div id="presentation">
+        <div class="presentContent">
+
+    <h3> Présentation du projet </h3> 
+    <p style="font-size: 150%;"> Le projet Envol CESSNA, réalisé dans le cadre d’une unité d’enseignements transdisciplinaires, 
+        répond à la volonté de l’Ecole Nationale Supérieure de Cognitique (ENSC) de disposer d’un simulateur 
+        d’aviation civile fonctionnel. Il a déjà été manipulé par différents groupes d’étudiants il y a 
+        quelques années, mais est inutilisé depuis. 
+    </p>
+</div>
+<hr>
+<div class="presentContent">
+
+    <h3> Présentation du simulateur </h3>
+    <p style="font-size: 150%;"> Voici un aperçu du simulateur se trouvant à la forge (bâtiment Est de l’ENSC). 
+        Il est composé d’un cockpit avec un palonnier, les commandes manuelles, un écran placé dans le 
+        cockpit servant à afficher le tableau de bord de l’avion durant le vol et un grand écran incurvé 
+        servant à afficher la vue de l’extérieur. 
+        <br>
+        <div class="centre">
+            <img src="images/simu.png" alt="Photo du simulateur">
+        </div>
+
+    </p>
+</div>
+<hr>
+<div class="presentContent">
+
+    <h3> Nos objectifs </h3>
+   
+</div>
+    </div>
+</body>
