8ecf677947098b2bc8db0fed66dc13cfbd103ec9
tristangclvs <goncalvestristan.pro@pm.me>
2022-05-20 14:54:04 +0200

    rouge bootstrap mis /

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="container-fluid footer position-fixed bottom-0 p-2">
         <div class="row">
-            <div class="col text-white text-center" style="font-size:large !important;"> Fait avec <i class="fa-solid fa-hand-holding-heart"></i> par l'équipe Envol Cessna.</div>
+            <div class="col text-white text-center" style="font-size:large !important;"> Fait avec <i class="fa-solid fa-hand-holding-heart" alt="amour"></i> par l'équipe Envol Cessna.</div>
         </div>
     </div>
 </footer>
