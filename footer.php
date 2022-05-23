0493812cfbf6fb9fbbf404bdbb822daa5f77383e
tristangclvs <goncalvestristan.pro@pm.me>
2022-05-23 16:55:04 +0200

    changements footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="container-fluid footer position-fixed bottom-0 p-2">
         <div class="row">
-            <div class="col text-white text-center" style="font-size:large !important;"> Fait avec <i class="fa-solid fa-hand-holding-heart" alt="amour"></i> par l'équipe Envol Cessna.</div>
+            <div class="col text-white text-center" style="font-size:large !important;"> Codé à la main et avec <i class="fa-solid fa-hand-holding-heart" alt="amour"></i> par l'équipe Envol Cessna, en appliquant les enseignements de l'école.</div>
         </div>
     </div>
 </footer>
